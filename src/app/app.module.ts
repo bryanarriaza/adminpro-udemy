e1649d16dde66d3bed4883f02b47d53d1b4ada46
Bryan Arriaza <bjarriaza.g.07@gmail.com>
2019-01-24 11:33:52 -0600

    Utilizacion de Inputs, Outputs, ViewChild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { APP_ROUTES } from './app.routes';
 
 //Modulos
 import { PagesModule } from './pages/pages.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -20,7 +21,8 @@ import { RegisterComponent } from './login/register.component';
   imports: [
     BrowserModule,
     APP_ROUTES,
-    PagesModule
+    PagesModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/incrementador/incrementador.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-incrementador',
+  templateUrl: './incrementador.component.html'
+})
+export class IncrementadorComponent implements OnInit {
+
+  @ViewChild('txtProgress') txtProgress: ElementRef;
+  @Input('nombre') leyenda: string = 'Leyenda';
+  @Input() progreso: number = 50;
+
+  @Output() cambioValor: EventEmitter<number> = new EventEmitter();
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  actChange( newValue: number ) {
+    if ( newValue >= 100 ) {
+      this.progreso = 100;
+    } else if ( newValue <= 0 ) {
+      this.progreso = 0;
+    } else {
+      this.progreso = newValue;
+    }
+    this.txtProgress.nativeElement.value = this.progreso;
+    this.cambioValor.emit(this.progreso);
+  }
+
+  cambiarValor( valor: number ) {
+    if ( this.progreso + valor >= 105 ) {
+      return;
+    } else if ( this.progreso + valor <= -5 ) {
+      return;
+    }
+    this.progreso += valor;
+    this.cambioValor.emit(this.progreso);
+    this.txtProgress.nativeElement.focus();
+  }
+
+}
